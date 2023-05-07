e864b0c39407e31bb226bd0879fc3247f656a0a9
Kenji MINOURA <info@kandj.org>
2023-05-07 09:38:19 +0900

    doc

--- mix.exs
+++ mix.exs
@@ -1,13 +1,28 @@
 defmodule Limelix.MixProject do
   use Mix.Project
 
+  @description"""
+  LIne MEssaging platform on ELIXer.
+  """
+
   def project do
     [
       app: :limelix,
       version: "0.1.0",
       elixir: "~> 1.14",
+      description: @description,
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+
+  def package do
+    [
+      maintainers: ["kjsd"],
+      licenses: ["BSD-2-Clause license"],
+      links: %{ "Github": "https://github.com/kjsd/limelix" }
     ]
   end
 
@@ -22,6 +37,7 @@ defmodule Limelix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:line_developers, "~> 0.1.0"},
       {:rpcsdk, "~> 0.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
