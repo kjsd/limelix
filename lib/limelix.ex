1371b269dac6c7a81ebcad24dd13da68ef58b360
Kenji MINOURA <info@kandj.org>
2023-05-07 20:41:38 +0900

    publish

--- lib/limelix.ex
+++ lib/limelix.ex
@@ -11,6 +11,18 @@ defmodule Limelix do
   alias Limelix.Skeletons.Users
   alias Limelix.Skeletons.Richmenus
 
+
+  def reply(access_token, reply_token, [_|_] = messages, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(reply_token) do
+    Replies.Queue.enqueue(oa_id, {
+          self(), {:reply!, [access_token, reply_token, messages]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
   def push(access_token, to, [_|_] = messages, options \\ [],
     oa_id \\ "LIMELIX_LINEOA")
   when is_binary(access_token) and is_binary(to) and is_list(options) do
@@ -53,6 +65,151 @@ defmodule Limelix do
     Narrowcasts.MessageQueue.enqueue(oa_id, {
           self(), {:narrowcast!, [
                       access_token, messages, recipient, filter, limit, options]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def progress_narrowcast(access_token, request_id, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(request_id) do
+    Narrowcasts.RequestQueue.enqueue(oa_id, {
+          self(), {:progress_narrowcast!, [
+                      access_token, request_id]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def create_audience(access_token, description, [_|_] = uid, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(description) do
+    Narrowcasts.AudienceQueue.enqueue(oa_id, {
+          self(), {:create_audience!, [
+                      access_token, description, uid]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def merge_audience(access_token, audience_id, description, [_|_] = uid,
+    oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_integer(audience_id) and is_binary(description) do
+    Narrowcasts.AudienceQueue.enqueue(oa_id, {
+          self(), {:merge_audience!, [
+                      access_token, audience_id, description, uid]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def delete_audience(access_token, audience_id, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_integer(audience_id) do
+    Narrowcasts.AudienceQueue.enqueue(oa_id, {
+          self(), {:delete_audience!, [
+                      access_token, audience_id]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def create_richmenu(access_token, richmenu, image_url, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_map(richmenu) and is_binary(image_url) do
+    Richmenus.ContentQueue.enqueue(oa_id, {
+          self(), {:create_richmenu!, [
+                      access_token, richmenu, image_url]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def delete_richmenu(access_token, richmenu_id, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(richmenu_id) do
+    Richmenus.ContentQueue.enqueue(oa_id, {
+          self(), {:delete_richmenu!, [
+                      access_token, richmenu_id]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def get_richmenu(access_token, richmenu_id, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(richmenu_id) do
+    Richmenus.RequestQueue.enqueue(oa_id, {
+          self(), {:get_richmenu!, [
+                      access_token, richmenu_id]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def alias_richmenu(access_token, richmenu_id, alias_id, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(richmenu_id) and is_binary(alias_id) do
+    Richmenus.RequestQueue.enqueue(oa_id, {
+          self(), {:alias_richmenu!, [
+                      access_token, richmenu_id, alias_id]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def unalias_richmenu(access_token, alias_id, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(alias_id) do
+    Richmenus.ContentQueue.enqueue(oa_id, {
+          self(), {:unalias_richmenu!, [
+                      access_token, alias_id]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def link_richmenu(access_token, richmenu_id, [_|_] = to, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(richmenu_id) do
+    Richmenus.RequestQueue.enqueue(oa_id, {
+          self(), {:link_richmenu!, [
+                      access_token, richmenu_id, to]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def unlink_richmenu(access_token, [_|_] = to, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) do
+    Richmenus.RequestQueue.enqueue(oa_id, {
+          self(), {:unlink_richmenu!, [
+                      access_token, to]}})
+
+    receive do
+      res ->
+        res
+    end
+  end
+
+  def get_profile(access_token, user_id, oa_id \\ "LIMELIX_LINEOA")
+  when is_binary(access_token) and is_binary(user_id) do
+    Users.Queue.enqueue(oa_id, {
+          self(), {:get_profile!, [
+                      access_token, user_id]}})
 
     receive do
       res ->
--- test/limelix_test.exs
+++ test/limelix_test.exs
@@ -5,6 +5,12 @@ defmodule LimelixTest do
   doctest Limelix
 
   describe "Limelix queue helpers" do
+    test "reply/4" do
+      assert res =  Limelix.reply("TOKEN", "REPLYTOKEN", [%{}])
+
+      assert match?({:ok, _}, res)
+    end
+
     test "push/5" do
       assert res =  Limelix.push("TOKEN", "UID", [%{}])
 
@@ -25,6 +31,78 @@ defmodule LimelixTest do
 
     test "narrowcast/6" do
       assert res =  Limelix.narrowcast("TOKEN", [%{}], %{}, %{}, %{})
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "progress_narrowcast/3" do
+      assert res =  Limelix.progress_narrowcast("TOKEN", "REQUEST_ID")
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "create_audience/4" do
+      assert res =  Limelix.create_audience("TOKEN", "DESC", ["UID"])
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "merge_audience/5" do
+      assert res =  Limelix.merge_audience("TOKEN", 999, "DESC", ["UID"])
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "delete_audience/3" do
+      assert res =  Limelix.delete_audience("TOKEN", 999)
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "create_richmenu/3" do
+      assert res =  Limelix.create_richmenu("TOKEN", %{}, "http://httpbin.org/status/200")
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "delete_richmenu/3" do
+      assert res =  Limelix.delete_richmenu("TOKEN", "RICHMENUID")
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "get_richmenu/3" do
+      assert res =  Limelix.get_richmenu("TOKEN", "RICHMENUID")
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "alias_richmenu/4" do
+      assert res =  Limelix.alias_richmenu("TOKEN", "RICHMENUID", "ALIASID")
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "unalias_richmenu/3" do
+      assert res =  Limelix.unalias_richmenu("TOKEN", "ALIASID")
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "link_richmenu/4" do
+      assert res =  Limelix.link_richmenu("TOKEN", "RICHMENUID", ["UID"])
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "unlink_richmenu/4" do
+      assert res =  Limelix.unlink_richmenu("TOKEN", ["UID"])
+
+      assert match?({:ok, _}, res)
+    end
+
+    test "get_profile/3" do
+      assert res =  Limelix.get_profile("TOKEN", "UID")
 
       assert match?({:ok, _}, res)
     end
